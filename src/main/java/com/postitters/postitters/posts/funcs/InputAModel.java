d1bccb078346bcb015739c641f1447dc8dbcba9d
BragaSoterasVinicius <viniciussoteras@gmail.com>
2023-10-14 21:37:39 -0300

    Interface frontend, exibição e sistema de postagem. Começa o desenvolvimento da ferramenta de pesquisa.

---
+++ src/main/java/com/postitters/postitters/posts/funcs/InputAModel.java
@@ -0,0 +1,13 @@
+package com.postitters.postitters.posts.funcs;
+
+public class InputAModel {
+    private String textInput;
+
+    public String getTextInput() {
+        return textInput;
+    }
+
+    public void setTextInput(String textInput) {
+        this.textInput = textInput;
+    }
+}
