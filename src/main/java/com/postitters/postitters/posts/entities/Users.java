f921985fce3d0489eef30eb897929504ff7fd024
Vinicius Soteras Braga <vinicius.braga@odontoprev.com.br>
2023-11-08 16:05:53 -0300

    Teste implantado - UserCatcher findAll

--- src/main/java/com/postitters/postitters/posts/entities/Users.java
+++ src/main/java/com/postitters/postitters/posts/entities/Users.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "users")
 public class Users {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ARROBA")
--- src/test/java/com/postitters/postitters/posts/service/UserCatcherTest.java
+++ src/test/java/com/postitters/postitters/posts/service/UserCatcherTest.java
@@ -1,9 +1,16 @@
 package com.postitters.postitters.posts.service;
 
+import com.postitters.postitters.posts.entities.Users;
+import com.postitters.postitters.posts.repo.UserRepo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class UserCatcherTest {
 
@@ -13,9 +20,25 @@ class UserCatcherTest {
 
     @Test
     void allUsers() {
+        UserRepo repoUser = mock(UserRepo.class);
+        UserCatcher userCatcher = new UserCatcher(repoUser);
+        List<Users> finalUsers = new ArrayList<>();
+        Users users0 = new Users();
+        Users users1 = new Users();
+
+        users0.setArroba("@ArrobaZero");
+        users1.setArroba("@ArrobaUm");
+
+        finalUsers.add(users0);
+        finalUsers.add(users1);
+
+        when(repoUser.findAll()).thenReturn(finalUsers);
+
+        assertEquals(UserCatcher.AllUsers(), finalUsers, "O retorno do repositório e o retorno do UserCatcher.findAll é diferente.");
     }
 
     @Test
     void getUserById() {
+
     }
 }
